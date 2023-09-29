94a646d6f827909f41ef319e7ced81a4a82d75fb
Ville Kalliomäki <i@v-k.fi>
2023-09-29 20:37:25 +0300

    Unused test utils removed

--- server/src/test_utils/database.rs
+++ server/src/test_utils/database.rs
@@ -1,25 +1,6 @@
 use sqlx::PgPool;
 
-use crate::{models::user::User, pg, settings};
-
-// Creates a database connection pool based on the settings
-// Also wiped the database, so each test can start with a clean db
-pub async fn clean_sqlx_pool() -> PgPool {
-    let pool = pg::create_pool(&settings::build().database_url).await;
-
-    // Should have a clearing function for each database table:
-    clear_users(&pool).await;
-
-    pool
-}
-
-// Deletes all users
-pub async fn clear_users(pool: &PgPool) {
-    sqlx::query!("DELETE FROM users")
-        .execute(pool)
-        .await
-        .unwrap();
-}
+use crate::models::user::User;
 
 // Create a test user
 pub async fn test_user(pool: &PgPool) -> User {
